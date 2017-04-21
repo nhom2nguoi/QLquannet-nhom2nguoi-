bba9c40e145733f2a450c6492e206f2c3574487b
nhom2nguoi <15i1leduchieu@gmail.com>
2017-04-22 00:03:38 +0700

    them thong tin

---
+++ ProjectManagment/Client/src/structure/MyProcess.java
@@ -0,0 +1,26 @@
+
+package structure;
+
+import java.io.Serializable;
+
+public class MyProcess implements Serializable{
+    
+    private String imagename;
+    private String pid;
+
+    public String getImagename() {
+        return imagename;
+    }
+
+    public void setImagename(String imagename) {
+        this.imagename = imagename;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+}
